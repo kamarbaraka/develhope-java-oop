bc32f041ec9e7987dd15f7727157c3c1fda842ae
kamarbaraka <kamar254baraka@gmail.com>
2023-06-13 17:57:07 +0300

    print your name added
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ src/main/java/co/develhope/introduction/PrintYourName.java
@@ -0,0 +1,9 @@
+package co.develhope.introduction;
+
+public class PrintYourName {
+
+    public static void main(String... args){
+
+        System.out.println("kamar");
+    }
+}
--- src/main/java/co/develhope/introduction/Start.java
+++ src/main/java/co/develhope/introduction/Start.java
@@ -1,2 +1,13 @@
-package co.develhope.introduction;public class Start {
+package co.develhope.introduction;
+
+import java.time.LocalDate;
+
+public class Start {
+
+    public static void main(String... args){
+
+        var birthday = LocalDate.of(1999, 8, 22);
+
+        System.out.println("Hello Java Student " + birthday);
+    }
 }
