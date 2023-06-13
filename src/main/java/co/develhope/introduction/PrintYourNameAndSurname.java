49f6a1273ea0da429536360cc4e239b02b731b95
kamarbaraka <kamar254baraka@gmail.com>
2023-06-13 18:00:34 +0300

    PrintYourNameAndSurname added
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ src/main/java/co/develhope/introduction/PrintYourNameAndSurname.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction;
+
+public class PrintYourNameAndSurname {
+
+    public static void main(String... args){
+
+        System.out.print("kamar ");
+        System.out.print("baraka");
+    }
+}
