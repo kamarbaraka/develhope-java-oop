e1695b38127702115b25815ade1a3226a507f5fd
kamarbaraka <kamar254baraka@gmail.com>
2023-06-13 20:50:09 +0300

    added ForLoop
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ src/main/java/co/develhope/introduction/ForLoop.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction;
+
+public class ForLoop {
+
+    public static void main(String... args){
+
+        for (int i = 5; i < 26; i++)
+            System.out.printf("%s ", i);
+    }
+}
