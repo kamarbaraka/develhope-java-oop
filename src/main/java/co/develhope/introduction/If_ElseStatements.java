c813dc714122cd8f1e2e181b0e2bc02394f8df9b
kamarbaraka <kamar254baraka@gmail.com>
2023-06-13 19:27:33 +0300

    added If_ElseStatements
    Signed-off-by: Kb_hacks <kamar254baraka@gmail.com>

---
+++ src/main/java/co/develhope/introduction/If_ElseStatements.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction;
+
+public class If_ElseStatements {
+
+    public static void main(String... args){
+        var myName = "kamar";
+
+        System.out.println((myName.length()%2 == 0)? "number of letters is even": "number of letters is odd");
+    }
+}
